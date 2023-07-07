439fe604f855066e4ec0d4ffd13e583b9d84ef33
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-07-07 18:22:31 +0530

    Update ECommerce.java

--- src/main/java/com/example/ecommerce/ECommerce.java
+++ src/main/java/com/example/ecommerce/ECommerce.java
@@ -1,25 +1,25 @@
 package com.example.ecommerce;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Ecommerce extends Application {
 
-    UserInterface userInterface = new UserInterface();
+    UserInterface userinterface =new UserInterface();
     @Override
     public void start(Stage stage) throws IOException {
-
-        Scene scene = new Scene(userInterface.createContent());
+        Scene scene = new Scene(userinterface.createContent());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }
